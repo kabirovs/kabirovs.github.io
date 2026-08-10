029ef88e6193ed986796a70af733a53b1b012b8a
saidovabdurahmon268 <saidovabdurahmon268@gmail.com>
2026-08-10 21:04:42 +0300

    Refactor showContacts function and add toggleMenu

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+function toggleMenu() {
+
+```
+const nav = document.getElementById("mainNav");
+
+nav.classList.toggle("active");
+```
+
+}
+
 function showContacts() {
 
+```
 const buttons =
-document.getElementById("contactButtons");
+    document.getElementById("contactButtons");
 
-if (
-buttons.style.display === "block"
-) {
+if (buttons.style.display === "flex") {
 
-buttons.style.display = "none";
+    buttons.style.display = "none";
 
 } else {
 
-buttons.style.display = "block";
+    buttons.style.display = "flex";
 
 }
+```
 
 }
