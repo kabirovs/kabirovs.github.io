bdb2bb9fd18951fe0dc550ce1a76840d855963bc
saidovabdurahmon268 <saidovabdurahmon268@gmail.com>
2026-08-10 20:41:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function toggleMenu() {
-    const nav = document.getElementById("mainNav");
-    nav.classList.toggle("active");
+function showContacts() {
+
+const buttons =
+document.getElementById("contactButtons");
+
+if (
+buttons.style.display === "block"
+) {
+
+buttons.style.display = "none";
+
+} else {
+
+buttons.style.display = "block";
+
 }
 
-function showContacts() {
-    const buttons = document.getElementById("contactButtons");
-
-    if (buttons.style.display === "flex") {
-        buttons.style.display = "none";
-    } else {
-        buttons.style.display = "flex";
-    }
 }
