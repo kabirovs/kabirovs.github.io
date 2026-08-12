a0a63638c1997f34f908736abbb768033fc09ff9
saidovabdurahmon268 <saidovabdurahmon268@gmail.com>
2026-08-12 09:13:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-```javascript
-function toggleMenu() {
-    const nav = document.getElementById("mainNav");
-    nav.classList.toggle("active");
-}
 
-function showContacts() {
-    const buttons = document.getElementById("contactButtons");
-
-    if (buttons.style.display === "flex") {
-        buttons.style.display = "none";
-    } else {
-        buttons.style.display = "flex";
-    }
-}
-```
